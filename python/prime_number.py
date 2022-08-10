a9b20f26832dea6d3ba29b61a460b1fe460a397f
00giri <kalin.toh.km@gmail.com>
2022-08-10 22:19:25 +0900

    add python-folder

---
+++ python/prime_number.py
@@ -0,0 +1,12 @@
+#エラトステネスの篩 - miniからmaxiまでの素数をリストで返す。
+
+def find_sosuu(mini, maxi):
+    sosuu = list(range(maxi+1))
+    sosuu2 = []
+    
+    for i in range(2, maxi+1):
+        if sosuu[i] != "-":
+            if i >= mini: sosuu2.append(sosuu[i])
+            for j in range(i*2,maxi+1,i):
+                sosuu[j] = "-"
+    return sosuu2
