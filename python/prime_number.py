561ba0f5b497b66777fa5b6de052f89e3e03de52
00giri <kalin.toh.km@gmail.com>
2023-03-06 22:34:09 +0900

    Update prime_number.py

--- python/prime_number.py
+++ python/prime_number.py
@@ -1,12 +1,12 @@
 #エラトステネスの篩 - miniからmaxiまでの素数をリストで返す。
 
-def primeNum(mini, maxi):
-    sosuu = list(range(maxi+1))
-    sosuu2 = []
-    
-    for i in range(2, maxi+1):
-        if sosuu[i] != "-":
-            if i >= mini: sosuu2.append(sosuu[i])
-            for j in range(i*2,maxi+1,i):
-                sosuu[j] = "-"
-    return sosuu2
+def primeNum2(mini, maxi):
+    prime = []
+    if mini <= 2: prime.append(2)
+    judge = [0 for _ in range(maxi+1)]
+    for i in range(3, maxi+1, 2):
+        if not judge[i]:
+            if i >= mini: prime.append(i)
+            for j in range(i*i, maxi+1, i):
+                judge[j] = 1
+    return prime
