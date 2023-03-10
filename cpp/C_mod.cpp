c83a9dd20f8534ffdc9572f4b807c9d64f11ba7e
00giri <kalin.toh.km@gmail.com>
2023-03-10 13:48:44 +0900

    Fix but in C_mod.cpp

--- cpp/C_mod.cpp
+++ cpp/C_mod.cpp
@@ -3,11 +3,13 @@ using namespace std;
 #define rep(i, s, n) for (int i = (s); i < (int)(n); i++)
 
 // nCr mod m modPowもセット
-long long modPow(int x, int n, int mod) {
+
+int modPow(int x, int n, int mod) {
     long long ans = 1;
+    long long x2 = x;
     while (n) {
-        if (n&1) ans = ans*x%mod;
-        x = x*x%mod;
+        if (n&1) ans = ans*x2%mod;
+        x2 = x2*x2%mod;
         n >>=1;
     }
     return ans;
@@ -19,17 +21,20 @@ struct Cmod {
     int mod;
 
     Cmod(int Nmax, int Mod) {
-        fct.resize(Nmax);
-        inv.resize(Nmax);
+        fct.resize(Nmax+1);
+        inv.resize(Nmax+1);
         mod = Mod;
 
         long long num = 1;
-        rep(i, 0, Nmax) {
-            num = num*(i+1)%mod;
+        fct[0] = 1;
+        rep(i, 1, Nmax+1) {
+            num = num*i%mod;
             fct[i] = num;
         }
 
         num = modPow(num, mod-2, mod);
+        inv[Nmax] = num;
+        inv[0] = 1;
         for (int i=Nmax; i>1; i--) {
             num = num*i%mod;
             inv[i-1] = num;
@@ -39,7 +44,7 @@ struct Cmod {
     int CMod(int n, int r) {
         r = min(r, n-r);
         if (r) {
-            return fct[n-1]*inv[r]%mod*inv[n-r]%mod;
+            return (long long)fct[n]*inv[r]%mod*inv[n-r]%mod;
         } else {
             return 1;
         }
