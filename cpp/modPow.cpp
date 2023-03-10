b668fcabdfb527752a21f865043e1384864b40e2
00giri <kalin.toh.km@gmail.com>
2023-03-10 13:47:46 +0900

    Fix bug in modPow.cpp

--- cpp/modPow.cpp
+++ cpp/modPow.cpp
@@ -2,11 +2,12 @@
 using namespace std;
 #define rep(i, s, n) for (int i = (s); i < (int)(n); i++)
 
-long long modPow(int x, int n, int mod) {
+int modPow(int x, int n, int mod) {
     long long ans = 1;
+    long long x2 = x;
     while (n) {
-        if (n&1) ans = ans*x%mod;
-        x = x*x%mod;
+        if (n&1) ans = ans*x2%mod;
+        x2 = x2*x2%mod;
         n >>=1;
     }
     return ans;
